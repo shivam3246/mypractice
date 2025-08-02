2b9568a2bd150fa4cf8d3bf691761c6738534c6a
Shivam Sharma <shivam@Shivams-MacBook-Air.local>
2025-08-02 14:33:39 +0530

    Solved using iterative dfs  approach handy for larger inputs

--- numberOfIslands.java
+++ numberOfIslands.java
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/number-of-islands/description/
+import java.util.*;
 public class numberOfIslands {
      public static int numIslands(char[][] grid) {
         int count = 0;
@@ -5,7 +7,7 @@ public class numberOfIslands {
             for(int j = 0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j);
+                    iterativeDfs(grid, i, j);
                 }
             }
         }
@@ -21,6 +23,28 @@ public class numberOfIslands {
         dfs(grid,i-1,j);
         dfs(grid,i,j+1);
         dfs(grid,i,j-1);
+    }
+    public static void iterativeDfs(char [][]grid,int i,int j){
+          int [][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
+          Stack<int []> stack = new Stack<>();
+          stack.push(new int[]{i,j});
+          while(!stack.isEmpty()){
+            int []cell = stack.pop();
+            int a = cell[0];
+            int b = cell[1];
+            if(a<0||a>=grid.length||b<0||b>=grid[0].length||grid[a][b]=='0'){
+                continue;
+            }
+            grid[a][b]='0';
+            
+
+            for(int []dir:directions){
+                int ni = a+dir[0];
+                int nj = b+dir[1];
+                stack.push(new int[]{ni,nj});
+            }
+
+          }
     }
     public static void main(String[] args) {
        char[][] grid = {
